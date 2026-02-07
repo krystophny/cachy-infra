6b26b94eb99810c1ef1e6611ff74b359267187ba
ert <ert@local>
2026-02-07 21:26:19 +0100

    Fix cycle-same-app to only switch on current desktop

--- scripts/cycle-same-app.sh
+++ scripts/cycle-same-app.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
-# Cycle through windows of the same application (like Cmd+` on macOS)
-# Requires: wmctrl, xprop
+# Cycle through windows of the same application on the current desktop (like Cmd+` on macOS)
+# Requires: wmctrl, xdotool, xprop
 
 active_id=$(printf '0x%08x' "$(xdotool getactivewindow)")
 active_class=$(xprop -id "$active_id" WM_CLASS | awk -F'"' '{print $4}')
+current_desktop=$(wmctrl -d | awk '$2 == "*" {print $1}')
 
-mapfile -t windows < <(wmctrl -lx | awk -v c="$active_class" '$3 ~ c {print $1}')
+mapfile -t windows < <(wmctrl -lx | awk -v c="$active_class" -v d="$current_desktop" '$3 ~ c && $2 == d {print $1}')
 
 [[ ${#windows[@]} -lt 2 ]] && exit 0
 
